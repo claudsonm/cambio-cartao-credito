c84f1000cc1576d5fa0885e24672aa9da5907058
Claudson Martins <claudson@outlook.com>
2019-11-24 09:57:53 -0300

    Adicionado autoprefixer

--- src/components/CurrencyConversor.vue
+++ src/components/CurrencyConversor.vue
@@ -2,21 +2,17 @@
 
   <div>
     <!-- https://wireframe.cc/T4TPR6 -->
-    <div class=" container mx-auto py-10">
-      <div class="border m-6 rounded-lg  bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
-        <div class="sm:flex sm:items-center px-6 py-4">
-          <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://api.adorable.io/avatars/196/abott@adorable.png" alt="">
-          <div class="text-center sm:text-left sm:flex-grow">
-            <div class="mb-4">
-              <p class="text-xl leading-tight">Jane Doe</p>
-              <p class="text-sm leading-tight text-grey-dark">Software Developer at SpongeBob LLC.</p>
-            </div>
-            <div class="flex flex-wrap">
-              <button class=" text-xs font-semibold rounded-full px-4 py-1 mx-3  leading-normal bg-white border border-blue text-blue hover:bg-blue hover:text-white">Call</button>
-              <button class="  text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Message</button>
-            </div>
-          </div>
-        </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 py-4">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
       </div>
     </div>
 
