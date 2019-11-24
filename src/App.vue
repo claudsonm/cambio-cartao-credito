da6240fd661fc319d99d38f31de4b42b7abeb978
Claudson Martins <claudson@outlook.com>
2019-11-24 02:06:09 -0300

    Adicionado tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld/>
+    <CurrencyConversor/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurrencyConversor from './components/CurrencyConversor.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CurrencyConversor
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
